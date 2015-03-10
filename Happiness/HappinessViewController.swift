c47842c4211627afcbaa309e144c7f5b98a5d528
Barry Martin <bymartin@verizon.net>
2015-03-10 15:15:20 -0500

    Initial Commit

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Barry Martin on 3/8/15.
+//  Copyright (c) 2015 BAMsoft. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController
+{
+
+
+
+}
